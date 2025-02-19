acc0e08b814f48e34aad7681123bcbd5714df09a
Rjk-Jami <rjkhan.1221@gmail.com>
2025-02-19 16:45:26 +0600

    search component with server comp

--- components/SearchInput/SearchInput.tsx
+++ components/SearchInput/SearchInput.tsx
@@ -1,33 +1,42 @@
-"use client";
-
 import { Search } from "lucide-react";
-import { useRouter } from "next/navigation";
-import React, { FormEvent, useState } from "react";
+import Form from "next/form";
+// import { useRouter } from "next/navigation";
+// import React, { FormEvent, useState } from "react";
 
 const SearchInput = () => {
-  const router = useRouter();
-  const [searchQuery, setSearchQuery] = useState("");
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (searchQuery.trim()) {
-      router.push(`/search/${encodeURIComponent(searchQuery.trim())}`);
-    }
-  };
+  // const router = useRouter();
+  // const [searchQuery, setSearchQuery] = useState("");
+
+  // const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   if (searchQuery.trim()) {
+  //     router.push(`/search/${encodeURIComponent(searchQuery.trim())}`);
+  //   }
+  // };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="relative w-full flex-1 max-w-[300px]"
-    >
+    // <form
+    //   onSubmit={handleSubmit}
+    //   className="relative w-full flex-1 max-w-[300px]"
+    // >
+    //   <input
+    //     type="text"
+    //     value={searchQuery}
+    //     onChange={(e) => setSearchQuery(e.target.value)}
+    //     placeholder="Search course.."
+    //     className=" w-full rounded-full bg-secondary/80 px-4 py-2 pl-10 text-sm focus:outline-none focus:ring-2 focus:ring-primary"
+    //   />
+    //   <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground"></Search>
+    // </form>
+    <Form action="/search" className="relative w-full flex-1 max-w-[300px]">
       <input
         type="text"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
+        name="courseQuery"
         placeholder="Search course.."
         className=" w-full rounded-full bg-secondary/80 px-4 py-2 pl-10 text-sm focus:outline-none focus:ring-2 focus:ring-primary"
       />
-      <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground"></Search>
-    </form>
+      <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground "></Search>
+    </Form>
   );
 };
 
