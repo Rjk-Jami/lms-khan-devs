a3555f54ff4333dfcce84e1e5506e2dd4efe3eb7
Rjk-Jami <rjkhan.1221@gmail.com>
2025-02-20 02:25:56 +0600

    add lesson type for sanity

---
+++ sanity/schemaTypes/module.ts
@@ -0,0 +1,22 @@
+import { defineType, defineField } from "sanity";
+
+export default defineType({
+  name: "module",
+  title: "Module",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (Rule) => Rule.required(), 
+    }),
+    defineField({
+      name: "lessons",
+      title: "Lessons",
+      type: "array", 
+      of: [{ type: "reference", to: [{ type: "lesson" }] }], 
+      validation: (Rule) => Rule.min(1), 
+    }),
+  ],
+});
