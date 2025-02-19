70cf3d4c9aa08fbea7892263b690efdefb76763f
Rjk-Jami <rjkhan.1221@gmail.com>
2025-02-20 04:04:20 +0600

    create course structure for sanity,

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,7 +1,19 @@
-import type {StructureResolver} from 'sanity/structure'
+import type { StructureResolver } from "sanity/structure";
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Content')
-    .items(S.documentTypeListItems())
+    .title("Admin Dashboard")
+    // S.documentTypeListItems()
+    .items([
+      S.listItem()
+        .title("Course Content")
+        .child(
+          S.documentTypeList("course")
+            .title("Courses")
+            .child((courseId) => S.list().title("Course Options").items([
+              S.listItem().title("Edit Course Content").child(S.document().schemaType("course").documentId(courseId)),
+              S.listItem().title("View Student").child(S.documentList().title("Course Enrollment").filter(`_type == "enrollment" && course._ref == $courseId`).params({courseId}))
+            ]))
+        ),
+    ]);
