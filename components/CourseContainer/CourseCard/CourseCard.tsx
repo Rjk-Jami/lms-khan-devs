435d69700385639aea71b0c858742c21c5727c93
Rjk-Jami <rjkhan.1221@gmail.com>
2025-02-22 02:40:49 +0600

    CourseCard complete

--- components/CourseContainer/CourseCard/CourseCard.tsx
+++ components/CourseContainer/CourseCard/CourseCard.tsx
@@ -4,6 +4,7 @@ import { urlFor } from "@/sanity/lib/image";
 import Image from "next/image";
 import Link from "next/link";
 
+// type safty
 interface CourseCardProps {
   course: GetCoursesQueryResult[number];
   progress?: number;
@@ -12,7 +13,7 @@ interface CourseCardProps {
 
 const CourseCard = ({ course, progress, href }: CourseCardProps) => {
   return (
-    <Link href={href} className="block">
+    <Link href={href} className="block group">
       <div className="bg-card border border-border rounded-xl overflow-hidden shadow-lg hover:shadow-xl transition-all duration-300 ease-in-out transform hover:-translate-x-1">
         <div className="relative h-52">
           {course.image ? (
@@ -22,7 +23,6 @@ const CourseCard = ({ course, progress, href }: CourseCardProps) => {
               fill
               className="object-fill"
             />
-
           ) : (
             <div className="h-full w-full flex items-center justify-center ">
               <Skeleton className="w-full h-full " />
@@ -32,25 +32,21 @@ const CourseCard = ({ course, progress, href }: CourseCardProps) => {
             <span className="rounded-full backdrop-blur-sm text-sm px-3 bg-black/50 text-white">
               {course?.category?.name}
             </span>
-            {
-              "price" in course && typeof course.price === "number" &&(
-                <span className="rounded-full backdrop-blur-sm text-sm px-3 bg-black/50 text-white">
-                  {course.price === 0 ? "Free" : `$${course.price.toLocaleString('en-BD')}`}
-                </span>
-              )
-            }
+            {"price" in course && typeof course.price === "number" && (
+              <span className="rounded-full backdrop-blur-sm text-sm px-3 bg-black/50 text-white">
+                {course?.price === 0 ? "Free" : `৳ ${course.price}`}
+              </span>
+            )}
           </div>
         </div>
-        <div className="p-4">
-          <h3 className="text-lg font-semibold text-primary truncate">{course.title}</h3>
-          {progress !== undefined && (
-            <div className="mt-2 h-2 w-full bg-gray-200 rounded-full">
-              <div
-                className="h-full bg-primary rounded-full transition-all"
-                style={{ width: `${progress}%` }}
-              ></div>
-            </div>
-          )}
+        <div className="p-6 flex flex-col flex-1">
+          <h3 className="text-xl font-bold mb-2  group-hover:text-primary transition-colors duration-300 truncate">
+            {course.title}
+          </h3>
+          <p className="text-muted-foreground mb-2 line-clamp-2 flex-1">
+            {course.description}
+          </p>
+          {progress !== undefined && <div className=""></div>}
         </div>
       </div>
     </Link>
